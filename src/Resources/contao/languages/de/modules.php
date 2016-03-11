34e8e6c547f5818cfe8c72db2199eb4d81f6ef37
Glen Langer <contao.modul@glen-langer.de>
2016-03-11 23:24:16 +0100

    Init - Contao 4 Version

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-grid_16_columns/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-06-08T22:51:20+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['grid_16_columns']['0'] = 'Contao Grid mit 16 Spalten';
+$GLOBALS['TL_LANG']['MOD']['grid_16_columns']['1'] = 'Contao Pixel und Prozent Grid mit 16 Spalten';
+
