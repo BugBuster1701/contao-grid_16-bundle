a8e2c2e53758573daff2d1ae7cdfc32c70d6e338
Glen Langer <contao.modul@glen-langer.de>
2017-05-06 00:08:52 +0200

    Umbau DCA eingener Abschnitt in tl_layout

--- src/Resources/contao/classes/Grid16Layout.php
+++ src/Resources/contao/classes/Grid16Layout.php
@@ -30,12 +30,35 @@ class Grid16Layout extends \PageRegular
      * Hook loadDataContainer
      * @param string $strName    dca
      */
-    public function addGrid16CSS(\PageModel $objPage, \LayoutModel &$objLayout, \PageRegular $objPageRegular)
+    public function addGrid16CSS(\PageModel $objPage, \LayoutModel $objLayout, \PageRegular $objPageRegular)
     {
-    	// $objLayout->getRelated('pid')
-    	
-        $GLOBALS['TL_CSS'][] = 'assets/contao-grid16/css/grid-1120-16-pixel.min.css';
-        log_message(print_r($objLayout,true),'grid16.log');
+        /*
+         * vor internen laden
+         * $GLOBALS['TL_CSS'][] = 'assets/contao-grid16/css/grid-1120-16-pixel.min.css';
+         * nach den anderen
+         * $GLOBALS['TL_HEAD']
+         */      
+        
+        $arrFrameworkGrid16 = \StringUtil::deserialize($objLayout->frameworkGrid16);
+        // Add the Grid16 CSS framework style sheets
+        if (is_array($arrFrameworkGrid16))
+        {
+            foreach ($arrFrameworkGrid16 as $strFile)
+            {
+                $GLOBALS['TL_HEAD'][] = 'bundles/bugbustergrid16/' . basename($strFile, '.css') . '.min.css';
+            }
+        }
+        
+    	//log_message(print_r($objLayout->framework,true),'grid16.log');
+        //log_message(print_r($objLayout->getRelated('pid'),true),'grid16.log');
+
+        /* geht nicht, da aus framework immer nur der basename genommen wird
+         * in PageRegular::createHeaderScripts
+            $arrValues = \StringUtil::deserialize($objLayout->frameworkGrid16, true);
+            $arrValues[] = 'assets/contao-grid16/css/grid-1120-16-pixel.min.css';
+            $objLayout->framework = serialize($arrValues);
+        */
+        log_message(print_r($objLayout->frameworkGrid16,true),'grid16.log');
         return $objLayout;
     }
 
