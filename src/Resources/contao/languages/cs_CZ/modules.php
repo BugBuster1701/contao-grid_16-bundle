95cc95103fc1dfbf74a315740e5cc81c291129c1
Glen Langer <contao.modul@glen-langer.de>
2018-12-26 01:15:00 +0100

    Fixed #2, Transifex, Übersetzungen aktualisieren (cs_CZ)

---
+++ src/Resources/contao/languages/cs_CZ/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-4-grid-16-columns-bundle/language/cs_CZ/
+ *
+ * last-updated: 2018-12-26T01:11:59+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['grid_16_columns']['0'] = 'Responzivní mřížka s 16 sloupci pro Contao';
+$GLOBALS['TL_LANG']['MOD']['grid_16_columns']['1'] = 'Responzivní mřížka v bodech nebo procentech s 16 sloupci';
+
---
+++ src/Resources/contao/languages/cs_CZ/tl_layout.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-4-grid-16-columns-bundle/language/cs_CZ/
+ *
+ * last-updated: 2018-12-26T01:11:59+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_layout']['after_internal']                              = 'Načíst soubor s 16-sloupcovou mřížkou po vnitřních stylech.';
+$GLOBALS['TL_LANG']['tl_layout']['before_framework']                            = 'Načíst soubor s 16-sloupcovou mřížkou před vnitřními styly.';
+$GLOBALS['TL_LANG']['tl_layout']['frameworkGrid16']['0']                        = 'Framework 16-sloupcové mřížky';
+$GLOBALS['TL_LANG']['tl_layout']['frameworkGrid16']['1']                        = 'Tady můžete aktivovat součásti tříd CSS 16-sloupcové mřížky';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive']['css']['0']   = '16-sloupcová mřížka, v bodech, 1120px, responzivní část';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive']['css']['1']   = 'Responzivní část 16-sloupcové mřížky, v bodech, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel']['css']['0']              = '16-sloupcová mřížka, v bodech, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel']['css']['1']              = 'Přidá 16-sloupcovou mřížku v bodech o velikosti 1120px, kterou lze ovládat pomocí tříd CSS "grid1" až "grid16" a "offset1" až "offset15".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive']['css']['0'] = '16-sloupcová mřížka, v procentech, 1140px, responzivní část';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive']['css']['1'] = 'Responzivní část 16-sloupcové mřížky, v procentech, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent']['css']['0']            = '16-sloupcová mřížka, v procentech, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent']['css']['1']            = 'Přidá 16-sloupcovou mřížku v procentech o velikosti 1140px, kterou lze ovládat pomocí tříd CSS "grid1" až "grid16" a "offset1" až "offset15".';
+$GLOBALS['TL_LANG']['tl_layout']['loadingOrderGrid16']['0']                     = 'Pořadí načítání';
+$GLOBALS['TL_LANG']['tl_layout']['loadingOrderGrid16']['1']                     = 'Zde můžete zvolit pořadí, jakým budou načteny soubory CSS';
+
